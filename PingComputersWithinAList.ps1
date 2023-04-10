196ef288e5f6f597cc2304aee589a86de14f3ee5
aperrygit <116298273+aperrygit@users.noreply.github.com>
2023-04-10 19:27:08 -0400

    Add files via upload

---
+++ PingComputersWithinAList.ps1
@@ -0,0 +1,9 @@
+﻿$computers = Get-Content -Path "C:\Users\apadmin\Documents\CommputerIPSV3_12_30_22.txt"
+ foreach ($computer in $computers)
+     {
+     $ip = $computer
+     if (Test-Connection  $ip -Count 1 -ErrorAction SilentlyContinue){
+         Write-Host "$ip is up"
+      
+     }
+     }
